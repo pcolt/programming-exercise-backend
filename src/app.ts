39ffa7256d02c7bd1d1540ed67b9862103b3ff7d
Pablo Colturi <pcolturi@fastmail.com>
2023-08-20 13:03:23 +0200

    add cors and simple rout to serve images

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
+import { log } from "console";
 import express, { Application, Request, Response } from "express";
+import path from "path";
+import cors from "cors";
 
 const app: Application = express();
-const port: number = 3001;
+const port: number = 3010;
+
+// Use the cors middleware to allow all origins
+app.use(cors());
+
+// Set the 'gifs' directory as the static folder
+app.use(express.static(path.join(__dirname, 'gifs')));
 
 app.get("/todo", (req: Request, res: Response) => {
   res.json({ message: "todo" });
+});
+
+// Define a route to display images
+app.get('/images/:imageName', (req: Request, res: Response) => {
+  const imageName = req.params.imageName;
+  res.sendFile(path.join(__dirname, 'gifs', imageName));
 });
 
 const server = app.listen(port);
